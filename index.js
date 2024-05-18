e389ba059f9077bc4782e806686482c39dd82de7
EngrArfin <arfin.cse.green.edu.bd@gmail.com@>
2024-05-18 18:21:29 +0600

    path parse

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+//Local-module
 const {a , add} = require('./local-1')
 const {a: a2 , add: add2} = require('./local-2')
 
 console.log(add2(3,4,6))
+
+
+//built-in-module
+const path = require("path")
+
+console.log(path.parse("C:/Projects2/node-js-practice/index.js"))
